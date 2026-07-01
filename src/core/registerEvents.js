da166755086f085da93fb3d131ac42a3d28d1d9b
StupieXD <stuartnegus@live.co.uk>
2026-07-01 12:51:17 +0100

    Update registerEvents.js

--- src/core/registerEvents.js
+++ src/core/registerEvents.js
@@ -1,9 +1,9 @@
 export async function registerEvents(client, events) {
   for (const event of events) {
     if (event.once) {
-      client.once(event.name, (...args) => event.execute(...args));
+      client.once(event.name, (...args) => event.execute(...args, client));
     } else {
-      client.on(event.name, (...args) => event.execute(...args));
+      client.on(event.name, (...args) => event.execute(...args, client));
     }
 
     console.log(`✅ Registered event: ${event.name}`);
