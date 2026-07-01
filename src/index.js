795f232b19df1b38088cdda700811c06007c3fd7
StupieXD <stuartnegus@live.co.uk>
2026-07-01 13:58:50 +0100

    Use event handler in startup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("Pank v2 starting...");
+import 'dotenv/config';
+
+import { createClient } from './core/client.js';
+import { loadEvents } from './handlers/eventHandler.js';
+import { validateConfig } from './config/validateConfig.js';
+import { config } from './config/config.js';
+
+validateConfig();
+
+const client = createClient();
+
+await loadEvents(client);
+
+await client.login(config.discordToken);
