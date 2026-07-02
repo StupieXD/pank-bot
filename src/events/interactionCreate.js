bfec4c66359be6c57684e047931f8e94ae299125
StupieXD <stuartnegus@live.co.uk>
2026-07-02 10:13:16 +0100

    Handle purge confirmation buttons

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -5,9 +5,20 @@ export const name = Events.InteractionCreate;
 export const once = false;
 
 export async function execute(interaction) {
-  if (!interaction.isChatInputCommand()) return;
+  if (interaction.isChatInputCommand()) {
+    if (interaction.commandName === 'purge') {
+      await purgeCommand.execute(interaction);
+    }
 
-  if (interaction.commandName === 'purge') {
-    await purgeCommand.execute(interaction);
+    return;
+  }
+
+  if (interaction.isButton()) {
+    if (
+      interaction.customId.startsWith('confirm_purge_') ||
+      interaction.customId.startsWith('cancel_purge_')
+    ) {
+      await purgeCommand.handlePurgeButton(interaction);
+    }
   }
 }
