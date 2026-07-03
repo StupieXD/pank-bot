6a752d630187af492c8d57e7434bd1ff392075db
StupieXD <stuartnegus@live.co.uk>
2026-07-03 12:40:25 +0100

    Add message delete event

--- src/events/messageDelete.js
+++ src/events/messageDelete.js
@@ -1,9 +1,9 @@
 import { Events } from 'discord.js';
-import { handleMessageDelete } from '../handlers/logging/handleMessageDelete.js';
+import { handleMessageDelete } from '../modules/messageLogger/messageDeleteLogger.js';
 
 export const name = Events.MessageDelete;
 export const once = false;
 
-export function execute(message) {
-  handleMessageDelete(message);
+export async function execute(message) {
+  await handleMessageDelete(message);
 }
