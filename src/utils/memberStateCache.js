b4e1e701e7f2e60c486e803bdc38eb81b577a4aa
StupieXD <stuartnegus@live.co.uk>
2026-07-04 17:50:30 +0100

    Add member state cache

---
+++ src/utils/memberStateCache.js
@@ -0,0 +1,24 @@
+const memberStateCache = new Map();
+
+function getCacheKey(guildId, userId) {
+  return `${guildId}:${userId}`;
+}
+
+export function getMemberState(member) {
+  return memberStateCache.get(getCacheKey(member.guild.id, member.id)) ?? null;
+}
+
+export function cacheMemberState(member) {
+  memberStateCache.set(getCacheKey(member.guild.id, member.id), {
+    nickname: member.nickname,
+    roleIds: new Set(
+      member.roles.cache
+        .filter((role) => role.id !== member.guild.id)
+        .map((role) => role.id)
+    )
+  });
+}
+
+export function deleteMemberState(member) {
+  memberStateCache.delete(getCacheKey(member.guild.id, member.id));
+}
