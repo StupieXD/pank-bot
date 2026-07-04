60787596a8a92e6954dbb08cf3c6bd8a128c47f5
StupieXD <stuartnegus@live.co.uk>
2026-07-04 17:19:39 +0100

    Add role change handler to member update event

--- src/events/guildMemberUpdate.js
+++ src/events/guildMemberUpdate.js
@@ -1,9 +1,11 @@
 import { Events } from 'discord.js';
 import { handleNicknameChange } from '../modules/memberLogger/nicknameChangeLogger.js';
+import { handleRoleChange } from '../modules/memberLogger/roleChangeLogger.js';
 
 export const name = Events.GuildMemberUpdate;
 export const once = false;
 
 export async function execute(oldMember, newMember) {
   await handleNicknameChange(oldMember, newMember);
+  await handleRoleChange(oldMember, newMember);
 }
