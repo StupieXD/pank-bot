eb222bcf359a50238e8f5e2fa2d21196652bbcca
StupieXD <stuartnegus@live.co.uk>
2026-07-13 01:30:01 +0100

    Update guildEmojiDelete.js

--- src/events/guildEmojiDelete.js
+++ src/events/guildEmojiDelete.js
@@ -1,7 +1,7 @@
 import { Events } from 'discord.js';
 import { handleEmojiDelete } from '../modules/serverLogger/emojiLogger.js';
 
-export const name = Events.EmojiDelete;
+export const name = Events.GuildEmojiDelete;
 export const once = false;
 
 export async function execute(emoji) {
