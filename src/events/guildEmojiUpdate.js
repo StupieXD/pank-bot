877cf21ed2b6dc00805baa19d9c8fb7090e9b8d0
StupieXD <stuartnegus@live.co.uk>
2026-07-13 01:20:59 +0100

    Update guildEmojiUpdate.js

--- src/events/guildEmojiUpdate.js
+++ src/events/guildEmojiUpdate.js
@@ -1,7 +1,7 @@
 import { Events } from 'discord.js';
 import { handleEmojiUpdate } from '../modules/serverLogger/emojiLogger.js';
 
-export const name = Events.GuildEmojiUpdate;
+export const name = Events.EmojiUpdate;
 export const once = false;
 
 export async function execute(oldEmoji, newEmoji) {
