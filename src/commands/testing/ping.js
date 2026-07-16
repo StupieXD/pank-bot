2676cad412c2249b56546a0827647bc8bc59c75e
StupieXD <stuartnegus@live.co.uk>
2026-07-16 18:07:35 +0100

    Delete src/commands/testing/ping.js

--- src/commands/testing/ping.js
+++
@@ -1,9 +0,0 @@
-import { SlashCommandBuilder } from 'discord.js';
-
-export const data = new SlashCommandBuilder()
-  .setName('ping')
-  .setDescription('Architecture test');
-
-export async function execute(interaction) {
-  await interaction.reply('🏓 Pong!');
-}
